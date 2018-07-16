711aa0d79aebfd0fd7a3e3e639ddd32db04ebef8
tom1mol <tomasomaolmhuaidh@gmail.com>
2018-07-16 18:46:18 +0000

    new commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
-/*$("p").click(function(){
-    $("p").slideToggle("slow");  // both disappear at same time
-})
-*/
-
 /*
-$("p").click(function(){
-    $(this).slideToggle("slow");  // both disappear individually using this keyword
+$("p").click(function(){            //this applied when we had 2 seperate paragraphs
+   // $("p").slideToggle("slow");  //slide/hides both <p>(paragraphs) when clicked on but doesnt reappear
+   $(this).slideToggle("slow");     // "this" makes each paragraph disappear seperately and they dont reappear
 })
-*/
 
+*/     /* paragraph slideToggle */
 /*
 $("button").mouseenter(function(){
     $(this).removeClass("makeRed").addClass("makeBlue");
@@ -17,35 +13,43 @@ $("button").mouseenter(function(){
 $("button").mouseleave(function(){
     $(this).removeClass("makeBlue").addClass("makeRed");
 });
-*/
+*/    /* mouseenter/leave makerRed/Blue */
 
 /*
 $(document).ready(function(){
-    $(".box").on("click", function(){        //box class and "on click function" 
-        var classNames = $(this).attr("class").split(" ");  // space is what we're spotting them by 
-        $("." + classNames[1]).css("background-color", "red"); //elements have 2 classes. box and 1   change bg color red
-    });   //split string box 1 whenever we encounter a space  achieved using split..gives us array containing 2 strings 
+    $(".box").on("click", function(){
+        var classNames = $(this).attr("class").split(" "); // space is what we are looking out for
+        $("." + classNames[1]).css("background-color", "red");
+        // elements have 2 classes. variables 
+        //assigned to string containing both classes(box and 1). for function to work
+        //split string box 1 wherever we encounter a space. achieved using split which gives an array
+        //containing 2 strings. box and 1
+    });
 });
-*/
 
-$(document).ready(function(){
+*/   /* splits 1, 2 , 3 with box example */
+
+$(document).ready(function(){   //use 'if' when we only want a certain class to change to red at given time
+    // use 'if' to check rgba value of element..if it's red..if =255,0,0 we change it to black
+    // this is done by setting background color to 0,0,0
+    // if value is not red..then we set all boxes to black(to ensure no other red boxes)
     $(".box").on("click", function(){
         /**
-         * when we click on an element that has
-         * a 'box class, this event will be fired.
+         * when we click an element that has 
+         * a 'box' class, this event will be fired.
          */
          var classNames = $(this).attr("class").split(" ");
          var boxClass = classNames[0];
          var className = classNames[1];
-         if ($(this).css("background-color") == "rgb(255, 0, 0)") {
-             //if this object already red...turn it black
+         if ($(this).css("background-color") =="rgb(255, 0, 0)") {
+             //if this object already red..turn it black
              $("." + className).css("background-color", "#000");
-        } else {
-            //else turn all elements with a box class black
-            //and then change the color of all elements
-            // with the same class name as the clicked element to red
-            $("." + boxClass).css("background-color", "#000");
-            $("." + className).css("background-color", "red");
-        }
+         } else {
+             //else turn all elements with a box class black
+             //and then change the color of all elements
+             //with the same class name as the clicked element to red
+             $("." + boxClass).css("background-color", "#000");
+             $("." + className).css("background-color", "red");
+         }
     });
-    });
+});
